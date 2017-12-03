c3d94873aa9109df8e7be81ab6d1bfa292eee986
Sarina <33553117+SarinaKarmacharya@users.noreply.github.com>
2017-12-03 14:27:18 -0500

    Update ADHD_fmri_find_series.m

--- ADHD_fmri_find_series.m
+++ ADHD_fmri_find_series.m
@@ -4,7 +4,7 @@ addpath /rfanfs/pnl-zorro/projects/ADHD/ADHD_PreProcess/
 caselist='/rfanfs/pnl-zorro/projects/ADHD/rsFMRI/scripts/rsfMRI_caselist.txt';
 files=textread(caselist,'%s');
 
-%dicom_path='/rfanfs/pnl-zorro/projects/ADHD/' filename{i} '/0_2015-07-06-17-17-28/4734849-843/2014.02.12-012Y-MR_Brain_w_o_Contrast-1455/SMS3_rs_fMRI-SMS3_rs_fMRI-19132'
+
 
 N = numel(files);
 
@@ -50,7 +50,7 @@ else
     caseDir.patient_number = cell2mat(folder_list);
     
     % STEP 3 => the main folder
-    % Example: 2014.07.01-009Y-MR_Functional_Imaging_Non_MD_60_Min-2105
+   
     pattern = '([0-9]+.)+[0-9]+-[0-9]+Y-[a-zA-Z_0-9]+-[0-9]+';
     
     folder_list = FindFolderFromRegex(fullfile(datapath, files{i}, caseDir.date_folder, caseDir.patient_number), pattern);
